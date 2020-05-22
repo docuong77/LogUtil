0566aa19df19a5a72df160114f7e938e697a308d
DO TRONG CUONG <docuong.hoc@gmail.com>
2020-05-22 10:16:37 +0700

    Message mô tả thay đổi

---
+++ logutil/src/main/java/com/example/logutil/LogDebug.java
@@ -0,0 +1,10 @@
+package com.example.logutil;
+
+import android.util.Log;
+
+public class LogDebug {
+private static final String TAG = "SUPER_AWESOME-APP";
+public static viod d(String message){
+    Log.d(TAG,message);
+}
+}
